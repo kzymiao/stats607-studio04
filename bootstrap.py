08aa6575e2692fc5e14941a6e0336c75a954bce5
eunice <eunicegu252@gmail.com>
2026-09-24 13:25:21 -0400

    Add boostrap.py and test_bootstrap.py

---
+++ bootstrap.py
@@ -0,0 +1,17 @@
+def bootstrap_sample():
+    """
+    implementation
+    """
+    ...
+
+def bootstrap_ci():
+    """
+    implementation
+    """
+    ...
+
+def r_squared():
+    """
+    implementation
+    """
+    ...
